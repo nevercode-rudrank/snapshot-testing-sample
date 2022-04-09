f4529b012bf6103ae9b53e3b3672dfb9fb17282f
Rudrank Riyam <rudrankriyam@gmail.com>
2022-04-10 04:50:23 +0530

    Increase subpixelThreshold to 5 from 3

--- SampleAppSnapshotTests/LoginViewControllerSnapshotTests.swift
+++ SampleAppSnapshotTests/LoginViewControllerSnapshotTests.swift
@@ -10,7 +10,7 @@ import XCTest
 import SnapshotTesting
 @testable import SampleApp
 
-let subpixelThreshold: UInt8 = 3
+let subpixelThreshold: UInt8 = 5
 
 class LoginViewControllerSnapshotTests: XCTestCase {
     
